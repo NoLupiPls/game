b547ed83abe4c8aa37e1d48a0fcf203e1fc766bc
Timofey <kalyakta@yandex.ru>
2025-01-20 14:15:01 +0700

    init в main

--- core/structure_platforms.py
+++ core/structure_platforms.py
@@ -19,10 +19,11 @@ class Block(pygame.sprite.Sprite):
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y, size, texture):
         super().__init__()
+        self.size = size
         self.image = pygame.image.load(texture)
-        self.image = pygame.transform.scale(self.image, (size, size))
+        self.image = pygame.transform.scale(self.image, (self.size, self.size))
         self.mask = pygame.mask.from_surface(self.image)
-        self.rect = pygame.Rect(x, y, x + size, y + size)
+        self.rect = pygame.Rect(x, y, x + self.size, y + self.size)
 
     def update_texture(self, new_texture):
         self.image = pygame.image.load(new_texture)
